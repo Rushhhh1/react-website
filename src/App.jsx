8112365b484242bf9bb486c3c762655d6473258f
rushika suram <rushika.r.suram@gmail.com>
2024-01-16 15:04:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./componen/hero";
+import Navigation from "./componen/navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
